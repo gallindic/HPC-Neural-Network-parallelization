abb7ac5ce619cf7847aad0664aba688d3dd4fd41
ozbej <ozbej.golob@gmail.com>
2023-05-31 00:17:40 +0200

    Minor change

--- mpi/run.sh
+++ mpi/run.sh
@@ -3,5 +3,5 @@
 #srun --reservation=fri-vr --partition=gpu mpicc -lm -o main main.c
 #srun --reservation=fri-vr --partition=gpu --mpi=pmix --nodes=1 --ntasks=2 ./main
 module load mpi/openmpi-4.1.3
-mpicc -fopenmp main.c -O2 -o main -lm -g -O0
-srun --time=00:10 --reservation=fri-vr --partition=gpu --mpi=pmix --nodes=2 --ntasks=8 main
+mpicc -fopenmp mlp.c -O2 -o mlp -lm -g -O0
+srun --time=00:10 --reservation=fri-vr --partition=gpu --mpi=pmix --nodes=2 --ntasks=16 mlp
